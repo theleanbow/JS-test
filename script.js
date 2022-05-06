de7499fcd3ae46b1f3651645cdebc9eaef395ce2
LeanBow <theleanbow@gmail.com>
2022-05-06 11:02:43 +0545

    Add capitalize string

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 
 const number = Math.floor(Math.random()*100+1);
 const number2 = Math.floor(Math.random()*100+1);
-
+const string = "Hello World";
 // add7
-function add7(){
+// function add7(){
     
-    console.log(number+7);
-}
-add7();
+//     console.log(number+7);
+// }
+// add7();
 
-function multiply(){
-    return number*number2;
-}
-console.log(multiply())
+// function multiply(){
+//     return number*number2;
+// }
+// console.log(multiply())
+
+// function capitalize(string){
+//     let capitalized=string.toUpperCase();
+//     return (capitalized);
+// }
+// capitalize(string);
